fddc975b5596b5877c86485d86fb0f3c5edd95ee
Krylov Sergey <kssergeyru@gmail.com>
2020-04-25 16:24:27 +0300

    Initial commit

---
+++ src/main/java/ru/aztest/aztest/AztestApplication.java
@@ -0,0 +1,13 @@
+package ru.aztest.aztest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AztestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AztestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/aztest/aztest/HelloWorldController.java
@@ -0,0 +1,32 @@
+package ru.aztest.aztest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/")
+    public String main() {
+        return "Усе работает";
+    }
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        // throw new RuntimeException("Some Error has Happened! Contact Support at
+        // ***-***");
+        return new HelloWorldBean("Hello Sergey");
+    }
+
+    /// hello-world/path-variable/in28minutes
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
